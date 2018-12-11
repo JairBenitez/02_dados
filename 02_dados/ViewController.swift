67b740b8dd7bb31a2d367ed6f4276776242441f0
Jair <jair@synapsis.com.mx>
2018-12-11 14:26:59 -0600

    Seleccionar valores random
    Seleccionar número aleatorio para el valor de los dados y mostrar la cara correspondiente

--- 02_dados/ViewController.swift
+++ 02_dados/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageViewDadoIzq: UIImageView!
     @IBOutlet weak var imageViewDadoDer: UIImageView!
     
+    var randomIndexDadoIzq : Int = 0
+    var randomIndexDadoDer : Int = 0
+    
+    let caras : [String] = ["dado1", "dado2", "dado3", "dado4", "dado5", "dado6" ]
+    let nCaras : UInt32
+    
+    
+    // Inicializador o constructor de la clase
+    required init?(coder aDecoder: NSCoder) {
+        
+        nCaras = UInt32(caras.count)
+        super.init(coder: aDecoder)
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +34,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func botonTirarPresionado(_ sender: UIButton) {
+        
+        generaRandomIndex()
     }
     
+    
+    // Genera indice aleatorio
+    func generaRandomIndex() {
+        
+        randomIndexDadoIzq = Int( arc4random_uniform(nCaras) )
+        randomIndexDadoDer = Int( arc4random_uniform(nCaras) )
+        
+        self.imageViewDadoIzq.image = UIImage( named: caras[randomIndexDadoIzq] )
+        self.imageViewDadoDer.image = UIImage( named: caras[randomIndexDadoDer] )
+        
+        print( randomIndexDadoDer )
+    }
 }
 
