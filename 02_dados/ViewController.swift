056452e54b61f19b236d41d19767440ca7f9d598
Jair <jair@synapsis.com.mx>
2018-12-11 13:57:07 -0600

    Se crea UI
    Se diseña la interfaz de usuario y se agregan las imágenes necesarias

--- 02_dados/ViewController.swift
+++ 02_dados/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageViewDadoIzq: UIImageView!
+    @IBOutlet weak var imageViewDadoDer: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func botonTirarPresionado(_ sender: UIButton) {
+    }
+    
 }
 
